af5d0452f538d07a2a78db221ca71309d90db066
Shinagawa-monkey <helena.shatalova.art@gmail.com>
2024-04-01 19:58:52 -0500

    Reverse to my fixed z-index for layout and components

--- src/components/Step.svelte
+++ src/components/Step.svelte
@@ -80,21 +80,21 @@
       {/if}
     </button> -->
 
-    <button on:click={toggleShowMore} aria-label={showMore ? "Show Less" : "Read More"} class="font-medium text-base md:text-lg poppins relative overflow-hidden px-3 py-1.5 rounded-full bg-violet-500 text-slate-950 cursor-pointer z-10 hover:bg-violet-600 duration-200 group/expandBtn focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-400 focus-visible:ring-opacity-50 whitespace-nowrap" tabindex={$isMenuOpen ? -1 : 0}>
-      <div class="absolute top-0 right-full w-full h-full bg-gray-200 opacity-20 group-hover/expandBtn:translate-x-full z-0 duration-200"></div>
+    <button on:click={toggleShowMore} aria-label={showMore ? "Show Less" : "Read More"} class="font-medium text-base md:text-lg poppins relative overflow-hidden px-3 py-1.5 rounded-full bg-violet-500 text-slate-950 cursor-pointer hover:bg-violet-600 duration-200 group/expandBtn focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-400 focus-visible:ring-opacity-50 whitespace-nowrap" tabindex={$isMenuOpen ? -1 : 0}>
+      <div class="absolute top-0 right-full w-full h-full bg-gray-200 opacity-20 group-hover/expandBtn:translate-x-full z-[0] duration-200"></div>
       {#if showMore}
-        <span>Show Less</span>
-        <span class="ml-1">&#8593;</span>
+        <span class="relative z-[1]">Show Less</span>
+        <span class="ml-1 relative z-[1]">&#8593;</span>
       {:else}
-        <span>Read More</span>
-        <span class="ml-1">&#8595;</span>
+        <span class="relative z-[1]">Read More</span>
+        <span class="ml-1 relative z-[1]">&#8595;</span>
       {/if}
     </button>
     
     
-    <a href={step.href} target="_blank" aria-label={"Go to " + step.name} class="font-medium text-base md:text-lg poppins relative overflow-hidden px-3 py-1.5 rounded-full bg-gray-100 text-slate-950 cursor-pointer z-10 hover:bg-gray-200 duration-200 group/GoToBtn focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-400 focus-visible:ring-opacity-50 whitespace-nowrap" tabindex={$isMenuOpen ? -1 : 0}>
-      <div class="absolute top-0 right-full w-full h-full bg-violet-800 opacity-20 group-hover/GoToBtn:translate-x-full z-0 duration-200"></div>
-      <h4>Go to &rarr;</h4>
+    <a href={step.href} target="_blank" aria-label={"Go to " + step.name} class="font-medium text-base md:text-lg poppins relative overflow-hidden px-3 py-1.5 rounded-full bg-gray-100 text-slate-950 cursor-pointer hover:bg-gray-200 duration-200 group/GoToBtn focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-400 focus-visible:ring-opacity-50 whitespace-nowrap" tabindex={$isMenuOpen ? -1 : 0}>
+      <div class="absolute top-0 right-full w-full h-full bg-violet-800 opacity-20 group-hover/GoToBtn:translate-x-full z-[0] duration-200"></div>
+      <h4 class="relative z-[1]">Go to &rarr;</h4>
     </a>
    
 
