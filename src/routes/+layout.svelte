cfef4b50ca09cfd4a5a488f0aee7e3dc66c41aaf
Shinagawa-monkey <helena.shatalova.art@gmail.com>
2024-04-04 01:20:55 -0500

    Header test solution 28

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -43,7 +43,7 @@
         <i class="fa-solid fa-arrow-up" />
     </button>
   </div>
-  <Header {y} {innerHeight} {theme}/>
+  <Header {y} {innerHeight} {theme} class="h-dvh" />
   <slot />
   <Footer />
 </div>
