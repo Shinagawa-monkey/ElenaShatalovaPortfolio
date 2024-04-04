02cd95096ce95d9fcf79f78b3c6752aef78d2e7e
Shinagawa-monkey <helena.shatalova.art@gmail.com>
2024-04-04 01:48:15 -0500

    Header neutral no solution

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -125,7 +125,8 @@
     </button>
   </div>
   <!-- Mobile Menu -->
-    <div bind:this={menu} class:flex={!isHidden} class:hidden={!isFlex} id="menu" class="fixed inset-0 z-[2] hidden self-end w-full m-h-screen opacity-90 bg-slate-950 md:hidden h-svh">
+  <div bind:this={menu} class:flex={!isHidden} class:hidden={!isFlex} id="menu" class="fixed inset-0 z-[2] hidden self-end w-full m-h-screen opacity-90 bg-slate-950 md:hidden h-svh">
+    <!-- <div bind:this={menu} class:flex={!isHidden} class:hidden={!isFlex} id="menu" class="fixed inset-0 z-[2] hidden self-end w-full h-full m-h-screen opacity-90 bg-slate-950 md:hidden"> -->
     <ul class="flex-col items-center px-4 py-1 pt-24 pb-4 tracking-widest text-white uppercase divide-y divide-slate-400">
       {#if !$page.error && $page.url.pathname === '/'}
         {#each tabs as tab, index}
