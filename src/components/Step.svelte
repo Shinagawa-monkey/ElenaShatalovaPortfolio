bec7ebf88161c8da28dfa12f6f6f59add053dff9
Shinagawa-monkey <helena.shatalova.art@gmail.com>
2024-04-12 02:27:51 -0500

    projects btns 4

--- src/components/Step.svelte
+++ src/components/Step.svelte
@@ -61,7 +61,7 @@
     <div class="flex flex-col gap-2">
       <ul class="flex flex-wrap gap-2">
         {#each step.technologies.split(', ') as tag}
-          <li class="bg-violet-500 dark:bg-violet-700 text-gray-900 dark:text-white px-3 py-1 rounded-full shadow-md text-sm sm:text-base whitespace-nowrap border-gray-800 dark:border-none">{tag}</li>
+          <li class="bg-violet-400 dark:bg-violet-700 text-gray-900 dark:text-white px-3 py-1 rounded-full shadow-md text-sm sm:text-base whitespace-nowrap border-gray-800 dark:border-none">{tag}</li>
         {/each}
       </ul>
     </div>
