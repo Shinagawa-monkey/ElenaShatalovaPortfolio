780462d805b79e753c12729e21dad6a8ae5449cd
Shinagawa-monkey <helena.shatalova.art@gmail.com>
2024-04-13 00:01:23 -0500

    projects btns

--- src/components/Step.svelte
+++ src/components/Step.svelte
@@ -37,7 +37,7 @@
   });
 </script>
 
-<div style="height: {height}" class="p-4 sm:p-6 md:p-8 flex flex-col gap-4 rounded-lg border border-solid border-violet-600 dark:border-violet-700 text-center cursor-default group hover:border-violet-800 dark:hover:border-violet-400 duration-200 transition-height">
+<div style="height: {height}" class="p-4 sm:p-6 md:p-8 flex flex-col gap-4 rounded-lg border border-solid border-violet-600 dark:border-violet-700 text-center cursor-default group hover:border-violet-900 dark:hover:border-violet-400 duration-200 transition-height">
   <div class="bg-blue-200 dark:bg-slate-950 grid place-items-center px-4 text-5xl md:text-6xl -mt-10 sm:-mt-12 md:-mt-14 lg:-mt-16 mx-auto duration-200">
     <i class={step.icon} />
   </div>
@@ -61,7 +61,7 @@
     <div class="flex flex-col gap-2">
       <ul class="flex flex-wrap gap-2">
         {#each step.technologies.split(', ') as tag}
-          <li class="bg-violet-400 dark:bg-violet-700 text-gray-950 dark:text-white px-3 py-1 rounded-full shadow-md text-sm sm:text-base whitespace-nowrap border-gray-800 dark:border-none">{tag}</li>
+          <li class="bg-violet-100 dark:bg-violet-700 text-violet-800 dark:text-white px-3 py-1 rounded-full shadow-md text-sm sm:text-base whitespace-nowrap border-gray-800 dark:border-none">{tag}</li>
         {/each}
       </ul>
     </div>
@@ -81,7 +81,7 @@
     </button> -->
 
     <button on:click={toggleShowMore} aria-label={showMore ? "Show Less" : "Read More"} class="font-medium text-base md:text-lg poppins relative overflow-hidden px-3 py-1.5 rounded-full bg-violet-600 dark:bg-violet-500 text-white dark:text-slate-950 cursor-pointer hover:bg-violet-700 dark:hover:bg-violet-600 duration-200 group/expandBtn focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-600 dark:focus-visible:ring-violet-400 focus-visible:ring-opacity-50 whitespace-nowrap" tabindex={$isMenuOpen ? -1 : 0}>
-      <div class="absolute top-0 right-full w-full h-full bg-violet-500 dark:bg-gray-200 opacity-10 dark:opacity-20 group-hover/expandBtn:translate-x-full z-[0] duration-200"></div>
+      <div class="absolute top-0 right-full w-full h-full bg-violet-200 dark:bg-gray-200 opacity-20 dark:opacity-20 group-hover/expandBtn:translate-x-full z-[0] duration-200"></div>
       {#if showMore}
         <span class="relative z-[1]">Show Less</span>
         <span class="ml-1 relative z-[1]">&#8593;</span>
@@ -90,26 +90,35 @@
         <span class="ml-1 relative z-[1]">&#8595;</span>
       {/if}
     </button>
-    <button on:click={toggleShowMore} aria-label={showMore ? "Show Less" : "Read More"} class="font-medium text-base md:text-lg poppins relative overflow-hidden px-3 py-1.5 rounded-full bg-violet-600 dark:bg-violet-500 text-white dark:text-slate-950 cursor-pointer hover:bg-violet-700 dark:hover:bg-violet-600 duration-200 group/expandBtn focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-600 dark:focus-visible:ring-violet-400 focus-visible:ring-opacity-50 whitespace-nowrap" tabindex={$isMenuOpen ? -1 : 0}>
-      <div class="absolute top-0 right-full w-full h-full bg-gray-200 opacity-20 group-hover/expandBtn:translate-x-full z-[0] duration-200"></div>
+
+
+    <button on:click={toggleShowMore} aria-label={showMore ? "Show Less" : "Read More"} class="font-medium text-base md:text-lg poppins relative overflow-hidden px-3 py-1.5 rounded-full bg-blue-500 text-white cursor-pointer hover:bg-blue-600 duration-200 group/expandBtn focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-400 focus-visible:ring-opacity-50 whitespace-nowrap" tabindex={$isMenuOpen ? -1 : 0}>
+      <div class="absolute top-0 right-full w-full h-full bg-gray-300 opacity-20 group-hover/expandBtn:translate-x-full z-[0] duration-200"></div>
       {#if showMore}
         <span class="relative z-[1]">Show Less</span>
-        <span class="ml-1 relative z-[1]">&#8593;</span>
+        <span class="ml-1 relative z-[1]">↑</span>
       {:else}
         <span class="relative z-[1]">Read More</span>
-        <span class="ml-1 relative z-[1]">&#8595;</span>
+        <span class="ml-1 relative z-[1]">↓</span>
       {/if}
     </button>
+    
     
     
     <a href={step.href} target="_blank" aria-label={"Go to " + step.name} class="font-medium text-base md:text-lg poppins relative overflow-hidden px-3 py-1.5 rounded-full bg-white dark:bg-gray-100 text-violet-600 dark:text-slate-950 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-200 duration-200 group/GoToBtn focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-600 dark:focus-visible:ring-violet-400 focus-visible:ring-opacity-50 whitespace-nowrap" tabindex={$isMenuOpen ? -1 : 0}>
-      <div class="absolute top-0 right-full w-full h-full bg-violet-200 dark:bg-violet-800 opacity-10 dark:opacity-20 group-hover/GoToBtn:translate-x-full z-[0] duration-200"></div>
+      <div class="absolute top-0 right-full w-full h-full bg-violet-200 dark:bg-violet-800 opacity-20 dark:opacity-20 group-hover/GoToBtn:translate-x-full z-[0] duration-200"></div>
       <h4 class="relative z-[1]">Go to &rarr;</h4>
     </a>
     <a href={step.href} target="_blank" aria-label={"Go to " + step.name} class="font-medium text-base md:text-lg poppins relative overflow-hidden px-3 py-1.5 rounded-full bg-gray-100 text-violet-600 dark:text-slate-950 cursor-pointer hover:bg-gray-200 duration-200 group/GoToBtn focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-600 dark:focus-visible:ring-violet-400 focus-visible:ring-opacity-50 whitespace-nowrap" tabindex={$isMenuOpen ? -1 : 0}>
       <div class="absolute top-0 right-full w-full h-full bg-violet-800 opacity-20 group-hover/GoToBtn:translate-x-full z-[0] duration-200"></div>
       <h4 class="relative z-[1]">Go to &rarr;</h4>
     </a>
+
+    <a href={step.href} target="_blank" aria-label={"Go to " + step.name} class="font-medium text-base md:text-lg poppins relative overflow-hidden px-3 py-1.5 rounded-full bg-gray-200 text-blue-600 cursor-pointer hover:bg-gray-300 duration-200 group/GoToBtn focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-400 focus-visible:ring-opacity-50 whitespace-nowrap" tabindex={$isMenuOpen ? -1 : 0}>
+      <div class="absolute top-0 right-full w-full h-full bg-blue-700 opacity-20 group-hover/GoToBtn:translate-x-full z-[0] duration-200"></div>
+      <h4 class="relative z-[1]">Go to →</h4>
+    </a>
+    
    
 
   </div>
