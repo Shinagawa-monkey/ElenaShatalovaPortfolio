a5b1440c087636867bdf2c23d6dd96d2541f7d2c
Shinagawa-monkey <helena.shatalova.art@gmail.com>
2024-04-16 21:46:39 -0500

    main CTA options add

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -165,21 +165,32 @@
         <span class="relative z-[1]">Get in touch &rarr;</span>
     </a> -->
 
-    <a href="https://www.linkedin.com/in/elena-shatalova/" target="_blank" class="font-medium blueShadowLight dark:blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white dark:bg-gray-100 text-violet-700 dark:text-slate-900 cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-200 duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-700 dark:focus-visible:ring-violet-400 focus-visible:ring-opacity-50 whitespace-nowrap select-none" tabindex={$isMenuOpen ? -1 : 0} aria-label="LinkedIn Profile">
+    <a href="https://www.linkedin.com/in/elena-shatalova/" target="_blank" class="font-medium blueShadowLight dark:blueShadowDark mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white dark:bg-gray-100 text-violet-700 dark:text-slate-900 cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-200 duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-700 dark:focus-visible:ring-violet-400 focus-visible:ring-opacity-50 whitespace-nowrap select-none" tabindex={$isMenuOpen ? -1 : 0} aria-label="LinkedIn Profile">
       <div class="absolute top-0 right-full w-full h-full bg-violet-400 dark:bg-violet-800 opacity-20 group-hover:translate-x-full z-[0] duration-200"></div>
         <span class="relative z-[1]">Get in touch &rarr;</span>
     </a>
 
+    <br>
+    <br>
     <!-- Gemini -->
     <a href="https://www.linkedin.com/in/elena-shatalova/" target="_blank" class="font-medium lightGlow dark:darkGlow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-violet-700 dark:text-slate-950 cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-700 dark:focus-visible:ring-violet-400 focus-visible:ring-opacity-50 whitespace-nowrap select-none" tabindex={$isMenuOpen ? -1 : 0} aria-label="LinkedIn Profile">
       <div class="absolute top-0 right-full w-full h-full bg-violet-600 dark:bg-violet-400 opacity-20 group-hover:translate-x-full z-[0] duration-200"></div>
       <span class="relative z-[1]">Get in touch &rarr;</span>
     </a>
 
-    <!-- Chatgpt -->
-    <a href="https://www.linkedin.com/in/elena-shatalova/" target="_blank" class="font-medium halGlow dark:hadGlow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white dark:bg-gray-100 text-violet-700 dark:text-slate-900 cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-200 duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-700 dark:focus-visible:ring-violet-400 focus-visible:ring-opacity-50 whitespace-nowrap select-none" tabindex={$isMenuOpen ? -1 : 0} aria-label="LinkedIn Profile">
-      <div class="absolute top-0 right-full w-full h-full bg-violet-400 dark:bg-violet-800 opacity-20 group-hover:translate-x-full z-[0] duration-200"></div>
-        <span class="relative z-[1]">Get in touch &rarr;</span>
+    <br>
+    <br>
+    <!-- Gemini -->
+    <a href="https://www.linkedin.com/in/elena-shatalova/" target="_blank" class="font-medium halGlow dark:hadGlow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-violet-700 dark:text-slate-950 cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-700 dark:focus-visible:ring-violet-400 focus-visible:ring-opacity-50 whitespace-nowrap select-none" tabindex={$isMenuOpen ? -1 : 0} aria-label="LinkedIn Profile">
+      <div class="absolute top-0 right-full w-full h-full bg-violet-600 dark:bg-violet-400 opacity-20 group-hover:translate-x-full z-[0] duration-200"></div>
+      <span class="relative z-[1]">Get in touch &rarr;</span>
+    </a>
+    <br>
+    <br>
+    <!-- Copilot -->
+    <a href="https://www.linkedin.com/in/elena-shatalova/" target="_blank" class="font-medium glowL dark:glowD mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-violet-700 dark:text-slate-950 cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-700 dark:focus-visible:ring-violet-400 focus-visible:ring-opacity-50 whitespace-nowrap select-none" tabindex={$isMenuOpen ? -1 : 0} aria-label="LinkedIn Profile">
+      <div class="absolute top-0 right-full w-full h-full bg-violet-600 dark:bg-violet-400 opacity-20 group-hover:translate-x-full z-[0] duration-200"></div>
+      <span class="relative z-[1]">Get in touch &rarr;</span>
     </a>
     
   </div>
