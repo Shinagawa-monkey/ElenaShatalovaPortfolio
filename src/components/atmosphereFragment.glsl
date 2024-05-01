3261a6780b212adaa69ad40a6c4e73e8827e52ea
Shinagawa-monkey <helena.shatalova.art@gmail.com>
2024-05-01 00:25:13 -0500

    change Earth atmosphere color

--- src/components/atmosphereFragment.glsl
+++ src/components/atmosphereFragment.glsl
@@ -3,5 +3,5 @@ uniform int theme; // 0 for light, 1 for dark
 
 void main() {
     float intensity = pow(0.75 - dot(vertexNormal, vec3(0, 0, 1.0)), 2.0);
-    gl_FragColor = vec4(0.0, 0.35, 0.7, 1.0) * intensity;
+    gl_FragColor = vec4(0.3, 0.6, 1.0, 1.0) * intensity;
 }
