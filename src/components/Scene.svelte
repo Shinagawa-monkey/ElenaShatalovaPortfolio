ab89cb62546c2946795b45cf98b85c8ae150a53c
Shinagawa-monkey <helena.shatalova.art@gmail.com>
2024-05-02 00:03:39 -0500

    add OrbitControls to let user rotate Earth

--- src/components/Scene.svelte
+++ src/components/Scene.svelte
@@ -18,114 +18,40 @@
   const texture = useLoader(TextureLoader).load('/images/globe.jpg');
   let atmosphereFragmentShader = $theme === 'light' ? atmosphereFragmentShaderLight : atmosphereFragmentShaderDark
 
-  // let cursorPosition = { x: 0, y: 0 };
-  // let rotationX = 0;
-  // let rotationY = 0;
-  // let rotationSpeed = 0.06;
-  // let rotationMesh=0;
-
-  // console.log(rotationX,rotationY, rotationMesh,  rotationSpeed);
-
-  // useTask((delta) => {
-  //   // Update rotation based on mouse movement
-  //   rotationX += delta * (cursorPosition.y - window.innerHeight / 2) / window.innerHeight * Math.PI * 2 * rotationSpeed;
-  //   rotationY += delta * (cursorPosition.x - window.innerWidth / 2) / window.innerWidth * Math.PI * 2 * rotationSpeed;
-  //   rotationMesh += delta * 0.01;
-  // });
-
-  // const handleMouseMove = (event) => {
-  //     cursorPosition.x = event.clientX;
-  //     cursorPosition.y = event.clientY;
-  //   };
-
-  // onMount(() => {
-  //   window.addEventListener('mousemove', handleMouseMove);
-  // });
-  // onDestroy(() => {
-  //   window.removeEventListener('mousemove', handleMouseMove);
-  // });
-
-  // let cursorPosition = { x: 0, y: 0 };
-  let rotationX = 0;
-  let rotationY = 0;
   let rotationMesh = 0;
-  let rotationSpeed = 0.05;
+  let rotationSpeed = 0.1;
 
   useTask((delta) => {
-    // const centerX = window.innerWidth / 2;
-    // const centerY = window.innerHeight / 2;
-    // const directionX = cursorPosition.x < centerX ? -1 : 1;
-    // const directionY = cursorPosition.y < centerY ? -1 : 1;
-
-    // // Update rotation based on direction and speed
-    // rotationX += delta * directionY * rotationSpeed;
-    // rotationY += delta * directionX * rotationSpeed;
-
-
-    rotationMesh += delta * 0.02;
+    rotationMesh += delta * rotationSpeed;
   });
 
-  // const handleClick = (e) => {
-  //   // cursorPosition.x = (e.clientX / innerWidth) * 2 - 1;
-  //   // cursorPosition.y = -(e.clientY / innerHeight) * 2 + 1; 
-  //   cursorPosition.x = e.clientX;
-  //   cursorPosition.y = e.clientY;
-  //   console.log(`x: ${cursorPosition.x}`, `y: ${cursorPosition.y}`);
-  // };
- 
-  // onMount(() => {
-  //   window.addEventListener('click', handleClick);
-  // });
-
-  // onDestroy(() => {
-  //   window.removeEventListener('click', handleClick);
-  // });
-
-    $: {
+  $: {
     atmosphereFragmentShader = $theme === 'light' ? atmosphereFragmentShaderLight : atmosphereFragmentShaderDark;
-    // console.log('Current Theme:', $theme);
-    // console.log('Selected Fragment Shader:', atmosphereFragmentShader);
   }
-
-    // Update rotation based on reactive variables
-  //   $: {
-  //   rotationX += cursorPosition.y - window.innerHeight / 2;
-  //   rotationY += cursorPosition.x - window.innerWidth / 2;
-  //   rotationMesh += 0.01;
-  // }
-  let autoRotate = false;
-  let enableDamping = true;
-  let rotateSpeed = 1;
-  let zoomToCursor = false;
-  let zoomSpeed = 1;
-  let minPolarAngle = 0;
-  let maxPolarAngle = Math.PI;
-  let enableZoom = true;
 </script>
 
 <T.PerspectiveCamera makeDefault fov={75} position={[0, 0, 15]} far={1000} near={0.1} on:create={({ ref }) => {
-  ref.lookAt(0, 0, 0)
+  ref.lookAt(0, 1, 0)
 }}>
   <OrbitControls
-  {enableDamping}
-  {autoRotate}
-  {rotateSpeed}
-  {zoomToCursor}
-  {zoomSpeed}
-  {minPolarAngle}
-  {maxPolarAngle}
-  {enableZoom}
+  autoRotate={false}
+  enableDamping={true}
+  rotateSpeed={0.4}
+  dampingFactor={0.1}
+  enableZoom={false}
 />
 </T.PerspectiveCamera>
 
-<T.Group>
-  <T.Mesh rotation.y={rotationMesh}>
-    <T.SphereGeometry args={[7.5, 75, 75]} />
-    {#await texture then value}
-      <T.ShaderMaterial fragmentShader={fragmentShader} vertexShader={vertexShader} uniforms={{ globeTexture: { value } }} />
-    {/await}
-  </T.Mesh>
-</T.Group>
+<T.DirectionalLight intensity={1} position={[5, 5, 5]} />
+<T.AmbientLight intensity={0.3} />
+
+<T.Mesh rotation.y={rotationMesh}>
+  <T.SphereGeometry args={[7.5, 75, 75]} />
+  {#await texture then value}
+    <T.ShaderMaterial fragmentShader={fragmentShader} vertexShader={vertexShader} uniforms={{ globeTexture: { value } }} />
+  {/await}
+</T.Mesh>
+
 
 <T.Mesh scale={[1.1, 1.1, 1.1]}>
   <T.SphereGeometry args={[7.5, 75, 756]} />
