acbf3f51550b62af0ec527125df7f03cc8abc82a
Shinagawa-monkey <helena.shatalova.art@gmail.com>
2024-05-04 01:35:33 -0500

    add vercel analytics

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,3 +1,3 @@
 // import { dev } from '$app/environment';
-// import { inject } from '@vercel/analytics';
+import { inject } from '@vercel/analytics'
 // inject({ mode: dev ? 'development' : 'production' });
