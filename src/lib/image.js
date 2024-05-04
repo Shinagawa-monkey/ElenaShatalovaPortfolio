57b75f1cb56df7bea0529217222da15d1ce69045
Shinagawa-monkey <helena.shatalova.art@gmail.com>
2024-05-04 00:17:02 -0500

    add img optimization

--- src/lib/image.js
+++ src/lib/image.js
@@ -1,15 +1,26 @@
 import { dev } from '$app/environment';
  
-export function optimize(src, widths = [640, 960, 1280], quality = 90) {
-  if (dev) return src;
+// export function optimize(src, widths = [640, 960, 1280], quality = 90) {
+//   if (dev) return src;
  
-  return widths
-    .slice()
-    .sort((a, b) => a - b)
-    .map((width, i) => {
-      const url = `/_vercel/image?url=${encodeURIComponent(src)}&w=${width}&q=${quality}`;
-      const descriptor = i < widths.length - 1 ? ` ${width}w` : '';
-      return url + descriptor;
-    })
-    .join(', ');
-}
+//   return widths
+//     .slice()
+//     .sort((a, b) => a - b)
+//     .map((width, i) => {
+//       const url = `/_vercel/image?url=${encodeURIComponent(src)}&w=${width}&q=${quality}`;
+//       const descriptor = i < widths.length - 1 ? ` ${width}w` : '';
+//       return url + descriptor;
+//     })
+//     .join(', ');
+// }
+
+export function optimize(src, widths = [640, 960, 1280], quality = 90) {
+    if (dev) return src;
+  
+    return widths
+      .map(width => {
+        const url = `/_vercel/image?url=${encodeURIComponent(src)}&w=${width}&q=${quality}`;
+        return `${url} ${width}w`;
+      })
+      .join(', ');
+  }
