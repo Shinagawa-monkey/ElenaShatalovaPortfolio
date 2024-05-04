0b499702000b132197063ef5fb5539ca0cfe37e2
Shinagawa-monkey <helena.shatalova.art@gmail.com>
2024-05-04 00:11:10 -0500

    remove img optimization for globe texture

--- src/lib/image.js
+++ src/lib/image.js
@@ -1,5 +1,4 @@
 import { dev } from '$app/environment';
-import { TextureLoader, Texture } from 'three';
  
 export function optimize(src, widths = [640, 960, 1280], quality = 90) {
   if (dev) return src;
@@ -14,12 +13,3 @@ export function optimize(src, widths = [640, 960, 1280], quality = 90) {
     })
     .join(', ');
 }
-
-export function optimizeTexture(src, widths = [640, 960, 1280], quality = 90) {
-    if (dev) return new TextureLoader().load(src);
-  
-    const loader = new TextureLoader();
-    const textures = widths.map(width => loader.load(`/_vercel/image?url=${encodeURIComponent(src)}&w=${width}&q=${quality}`));
-    
-    return new Texture(textures);
-  }
